570207fc28583eecee2b1ce13fc9530cae8c8112
James Jung <therealjamesjung@gmail.com>
2022-09-22 15:31:15 +0900

    feat: Set up PostgreSQL connection

---
+++ app/database/base.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = 'postgresql://admin_15gg:admin_15gg@127.0.0.1:5432/db_15gg'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
