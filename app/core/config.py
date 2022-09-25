3b880ecd38f9a8bfc2cdad8a070ef2b6b2d16efa
James Jung <therealjamesjung@gmail.com>
2022-09-26 04:01:47 +0900

    feat: Set app config from .env file

---
+++ app/core/config.py
@@ -0,0 +1,24 @@
+import secrets
+
+from pydantic import BaseSettings
+from dotenv import dotenv_values
+
+env = dotenv_values('.env')
+
+
+DB_URI = 'postgresql://' + \
+    f'{env["DB_USERNAME"]}:{env["DB_PASSWORD"]}' + \
+    f'@{env["DB_HOST"]}:{env["DB_PORT"]}/{env["DB_NAME"]}'
+
+
+class Settings(BaseSettings):
+    API_ROOT: str = '/api/v1'
+    PROJECT_NAME: str = 'app_15gg'
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SQLALCHEMY_DATABASE_URI: str = DB_URI
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
