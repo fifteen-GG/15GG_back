87ddbd3c8946afccc9fecd60a609b2dfd7e67578
James Jung <therealjamesjung@gmail.com>
2022-09-27 01:56:45 +0900

    feat: Add code generate API

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.v1 import game
+from app.api.v1 import game, code
 
 api_router = APIRouter()
 api_router.include_router(game.router, prefix="/game", tags=["games"])
+api_router.include_router(code.router, prefix="/code", tags=["codes"])
