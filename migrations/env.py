2425497f278456424f3710c73b3bb5797dc746c7
James Jung <therealjamesjung@gmail.com>
2022-09-29 01:48:19 +0900

    style: Change double quotes to single

--- migrations/env.py
+++ migrations/env.py
@@ -5,27 +5,16 @@ from sqlalchemy import engine_from_config, pool
 from app.core import settings
 from app.database.base import Base
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
 target_metadata = Base.metadata
 
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
-
 def run_migrations_offline():
-    """Run migrations in 'offline' mode.
+    '''
+    Run migrations in 'offline' mode.
 
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
@@ -34,27 +23,26 @@ def run_migrations_offline():
 
     Calls to context.execute() here emit the given string to the
     script output.
-
-    """
+    '''
     url = settings.SQLALCHEMY_DATABASE_URI
     context.configure(url=url, target_metadata=target_metadata, literal_binds=True,
-                      dialect_opts={"paramstyle": "named"}, compare_type=True)
+                      dialect_opts={'paramstyle': 'named'}, compare_type=True)
 
     with context.begin_transaction():
         context.run_migrations()
 
 
 def run_migrations_online():
-    """Run migrations in 'online' mode.
+    '''
+    Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
-    """
+    '''
     configuration = config.get_section(config.config_ini_section)
-    configuration["sqlalchemy.url"] = settings.SQLALCHEMY_DATABASE_URI
+    configuration['sqlalchemy.url'] = settings.SQLALCHEMY_DATABASE_URI
     connectable = engine_from_config(
-        configuration, prefix="sqlalchemy.", poolclass=pool.NullPool)
+        configuration, prefix='sqlalchemy.', poolclass=pool.NullPool)
 
     with connectable.connect() as connection:
         context.configure(connection=connection,
