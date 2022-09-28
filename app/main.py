17251199d8594a4ed31de727cbb3f2fa1a885ae5
James Jung <therealjamesjung@gmail.com>
2022-09-29 01:48:33 +0900

    style: Change double quotes to single

--- app/main.py
+++ app/main.py
@@ -7,17 +7,17 @@ from app.api.v1 import api_router
 from app.core import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
-origins = ["*"]
+origins = ['*']
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 app.include_router(api_router, prefix=settings.API_ROOT)
 
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+if __name__ == '__main__':
+    uvicorn.run('app.main:app', host='0.0.0.0', port=8000, reload=True)
